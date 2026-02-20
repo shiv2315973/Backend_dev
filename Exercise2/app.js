9a1df0f47bf912982cfb3ebdc43ff4816a3d3248
Rishi Gupta <rishiguptaorai04@gmail.com>
2026-02-21 01:29:02 +0530

    Add Node.js Exercise2 - Custom module

---
+++ Exercise2/app.js
@@ -0,0 +1,5 @@
+const utils = require('./stringUtils');
+
+console.log("Capitalized:", utils.capitalize("nodejs"));
+console.log("Reversed:", utils.reverse("backend"));
+console.log("Vowel Count:", utils.countVowels("engineering"));
